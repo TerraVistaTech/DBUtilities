aa9d55712e1e09e0fc076ddc31090720fc2b2686
Jason Unger <entityreborn@gmail.com>
2014-08-19 13:17:25 -0700

    Added basic download functionality.

---
+++ Download.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeFile="Download.aspx.cs" Inherits="Download" %>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="content" Runat="Server">
+    <div>
+        <div id="download">
+
+            <asp:ListBox ID="lstBackupfiles" runat="server">
+            </asp:ListBox>
+
+            <br />
+
+            <asp:Button CssClass="btn" ID="btnDownload" runat="server" UseSubmitBehavior="true" Text="Download" onclick="btnDownload_Click" />
+        </div>
+    </div>
+    <p><asp:Label ID="lblMessage" ForeColor="Red" runat="server" Text=""></asp:Label></p>
+</asp:Content>
---
+++ Download.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Download : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!Page.IsPostBack)
+        {
+            ReadBackupFiles();
+        }
+    }
+
+    private void ReadBackupFiles()
+    {
+        try
+        {
+            Directory.CreateDirectory(ConfigurationManager.AppSettings["BackupDirectory"]);
+
+            string[] files = Directory.GetFiles(ConfigurationManager.AppSettings["BackupDirectory"], "*.bak");
+            lstBackupfiles.DataSource = files;
+            lstBackupfiles.DataBind();
+            lstBackupfiles.SelectedIndex = 0;
+        }
+        catch (Exception exception)
+        {
+            lblMessage.Text = exception.Message.ToString();
+        }
+    }
+
+    protected void btnDownload_Click(object sender, EventArgs e)
+    {
+        string _BackupName = lstBackupfiles.SelectedItem.Text;
+        FileInfo file = new FileInfo(_BackupName);
+
+        Response.Clear();
+
+        Response.ClearHeaders();
+
+        Response.ClearContent();
+
+        Response.AddHeader("Content-Disposition", "attachment; filename=" + file.Name);
+
+        Response.AddHeader("Content-Length", file.Length.ToString());
+
+        Response.ContentType = "application/octet-stream";
+
+        Response.Flush();
+
+        Response.TransmitFile(file.FullName);
+
+        Response.End();
+    }
+}
